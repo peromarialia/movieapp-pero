e10056425e1a1e1247d38f7cd9fdc0bc41d73faa
lia <peromarialia04@gmail.com>
2026-06-08 16:30:28 +0200

    Creata funzione di sicurezza

--- js/utils.js
+++ js/utils.js
@@ -42,3 +42,12 @@ function createCard(item) {
     </article>
   `;
 }
+
+function escapeHTML(value) {
+  return String(value)
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
+}
