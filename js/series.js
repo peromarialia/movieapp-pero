9afe2ab27c2997278983a1f7abf23611d29d7777
lia <peromarialia04@gmail.com>
2026-06-08 19:39:56 +0200

    Creazione della funzione principale e esecuzione

--- js/series.js
+++ js/series.js
@@ -2,3 +2,19 @@ import { getSeries } from "./api.js";
 import { renderCards, setStatus, hideStatus } from "./utils.js";
 
 const grid = document.querySelector("#content-grid");
+
+async function init() {
+  try {
+    setStatus("Caricamento serie...");
+
+    const series = await getSeries();
+
+    renderCards(series, grid);
+    hideStatus();
+  } catch (error) {
+    setStatus("Si è verificato un errore durante il caricamento delle serie.");
+    console.error(error);
+  }
+}
+
+init();
