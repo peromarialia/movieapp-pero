09ec500fa4570cd2ead76a12f1a1e6671b1cad79
lia <peromarialia04@gmail.com>
2026-06-08 18:54:48 +0200

    Aggiunto percorso json e creata la funzione che legge il catalogo

---
+++ js/api.js
@@ -0,0 +1,12 @@
+const LOCAL_DATA_URL = "./movies.json";
+
+async function getLocalCatalog() {
+  const response = await fetch(LOCAL_DATA_URL);
+
+  if (!response.ok) {
+    throw new Error("Errore nel caricamento del catalogo locale.");
+  }
+
+  const data = await response.json();
+  return data.items;
+}
