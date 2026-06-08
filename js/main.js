086d8f1c98c2f1374a3861babf3246ccef8c7b91
lia <peromarialia04@gmail.com>
2026-06-08 19:32:11 +0200

    Creazione della funzione principale e esecuzione della funzione

--- js/main.js
+++ js/main.js
@@ -2,3 +2,19 @@ import { getAllContent } from "./api.js";
 import { renderCards, setStatus, hideStatus } from "./utils.js";
 
 const grid = document.querySelector("#content-grid");
+
+async function init() {
+  try {
+    setStatus("Caricamento contenuti...");
+
+    const content = await getAllContent();
+
+    renderCards(content, grid);
+    hideStatus();
+  } catch (error) {
+    setStatus("Si è verificato un errore durante il caricamento dei contenuti.");
+    console.error(error);
+  }
+}
+
+init();
