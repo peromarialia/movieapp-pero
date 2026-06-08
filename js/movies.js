feea2e6c1ab628795ba05961bb447fc8532c56be
lia <peromarialia04@gmail.com>
2026-06-08 19:37:16 +0200

    Creazione della funzione principale e esecuzione della fuzione

--- js/movies.js
+++ js/movies.js
@@ -2,3 +2,19 @@ import { getMovies } from "./api.js";
 import { renderCards, setStatus, hideStatus } from "./utils.js";
 
 const grid = document.querySelector("#content-grid");
+
+async function init() {
+  try {
+    setStatus("Caricamento film...");
+
+    const movies = await getMovies();
+
+    renderCards(movies, grid);
+    hideStatus();
+  } catch (error) {
+    setStatus("Si è verificato un errore durante il caricamento dei film.");
+    console.error(error);
+  }
+}
+
+init();
